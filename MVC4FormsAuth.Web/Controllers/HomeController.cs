2ebc90e606a23c5063bade85f52f5982a685a981
Huan B <huan@bui.com>
2018-04-28 22:18:15 -0700

    initial commit

---
+++ MVC4FormsAuth.Web/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4FormsAuth.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            //var test = System.Web.HttpContext.Current.Request.ServerVariables["SERVER_PROTOCOL"].ToString();
+            return View();
+        }
+    }
+}
